790142402e33e1a5c10ab55b672eda60cfc89787
lhossainduet094026 <lhossainduet094026@gmail.com>
2025-10-14 21:16:54 +0600

    feat(first commit)

---
+++ src/main/java/com/lokman/securityHandsDirtry/SecurityHandsDirtryApplication.java
@@ -0,0 +1,13 @@
+package com.lokman.securityHandsDirtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHandsDirtryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHandsDirtryApplication.class, args);
+	}
+
+}

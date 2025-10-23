25b6770afd10528d5e6ef93610e8ce16b5636e83
lhossainduet094026 <lhossainduet094026@gmail.com>
2025-10-23 12:41:46 +0600

    remove white lebel page and added 401 and 403 handler for custom response.

--- src/main/java/com/lokman/securityHandsDirtry/config/WebSecurityConfig.java
+++ src/main/java/com/lokman/securityHandsDirtry/config/WebSecurityConfig.java
@@ -17,10 +17,6 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-//		http.authorizeHttpRequests(auth -> auth.requestMatchers("/**").hasAnyRole("USER"))
-//		.httpBasic(Customizer.withDefaults())
-//		.formLogin(form -> form.disable());
-		
 		/**
 		 * 
 		 * securing end point based on roles(Inmemory)
@@ -35,8 +31,40 @@ public class WebSecurityConfig {
 		// Any other request → authenticated users only
 		.anyRequest().authenticated())
 		.httpBasic(Customizer.withDefaults())
-		.formLogin(form -> form.disable());
+		.formLogin(form -> form.disable())
+		// 403 handler
+		.exceptionHandling(ex -> ex
+	            // 403 handler
+	            .accessDeniedHandler((request, response, ex1) -> {
+	                response.setStatus(response.SC_FORBIDDEN);
+	                response.setContentType("application/json");
 
-		return http.build();
+	                String jsonResponse = "{"
+	                        + "\"status\": 403,"
+	                        + "\"error\": \"Forbidden\","
+	                        + "\"message\": \"Access Denied: You do not have permission to view this resource\","
+	                        + "\"path\": \"" + request.getRequestURI() + "\""
+	                        + "}";
+	                
+	                response.getWriter().write(jsonResponse);
+	            })
+	            // 401 handler
+	            .authenticationEntryPoint((request, response, ex2) -> {
+	                response.setStatus(response.SC_UNAUTHORIZED);
+	                response.setContentType("application/json");
+
+	                String jsonResponse = "{"
+	                        + "\"status\": 401,"
+	                        + "\"error\": \"Unauthorized\","
+	                        + "\"message\": \"You must authenticate before accessing this resource\","
+	                        + "\"path\": \"" + request.getRequestURI() + "\""
+	                        + "}";
+
+	                response.getWriter().write(jsonResponse);
+	            })
+	        );
+
+	    return http.build();
+	    
 	}
 }

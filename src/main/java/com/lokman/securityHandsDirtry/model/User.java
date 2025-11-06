450c23f0ea31a6c467a06a3d873c4eeb0db4b02b
lhossainduet094026 <lhossainduet094026@gmail.com>
2025-11-06 15:53:07 +0600

    user model and spring data jpa dependency added.

---
+++ src/main/java/com/lokman/securityHandsDirtry/model/User.java
@@ -0,0 +1,34 @@
+package com.lokman.securityHandsDirtry.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+
+	private String password;
+
+	private String role;
+
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
